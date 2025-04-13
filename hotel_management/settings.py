045759e327bacec4aaec9b685a2c96a8e12c1809
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2025-04-13 15:08:34 +0000

    Change grid row size on booking page for image. chang size of the login and signup blocks

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["oceanshotel.up.railway.app"]
+ALLOWED_HOSTS = ["oceanshotel.up.railway.app", "*"]
 
 
 # Application definition
